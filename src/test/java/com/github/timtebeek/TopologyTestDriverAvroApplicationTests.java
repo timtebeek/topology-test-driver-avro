ac6e7e3fb92eb5cf0c15ab1d274d3095cc381666
Tim te Beek <tim.te.beek@jdriven.com>
2019-12-28 15:09:24 +0100

    Use MockSchemaRegistry as suggested by Matthias J. Sax

--- src/test/java/com/github/timtebeek/TopologyTestDriverAvroApplicationTests.java
+++ src/test/java/com/github/timtebeek/TopologyTestDriverAvroApplicationTests.java
@@ -5,10 +5,10 @@ import java.util.Properties;
 
 import example.avro.Color;
 import example.avro.User;
-import io.confluent.kafka.schemaregistry.client.MockSchemaRegistryClient;
+import io.confluent.kafka.schemaregistry.client.SchemaRegistryClient;
+import io.confluent.kafka.schemaregistry.testutil.MockSchemaRegistry;
 import io.confluent.kafka.serializers.AbstractKafkaAvroSerDeConfig;
 import io.confluent.kafka.streams.serdes.avro.SpecificAvroSerde;
-import org.apache.avro.specific.SpecificRecord;
 import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.common.serialization.Serdes.StringSerde;
@@ -22,33 +22,45 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class TopologyTestDriverAvroApplicationTests {
 
+	private static final String SCHEMA_REGISTRY_SCOPE = TopologyTestDriverAvroApplicationTests.class.getName();
+	private static final String MOCK_SCHEMA_REGISTRY_URL = "mock://" + SCHEMA_REGISTRY_SCOPE;
+
 	private TopologyTestDriver testDriver;
 
 	private TestInputTopic<String, User> usersTopic;
 	private TestOutputTopic<String, Color> colorsTopic;
 
 	@BeforeEach
-	void beforeEach() {
+	void beforeEach() throws Exception {
 		// Create topology to handle stream of users
 		StreamsBuilder builder = new StreamsBuilder();
 		new TopologyTestDriverAvroApplication().handleStream(builder);
 		Topology topology = builder.build();
+
+		// Create a mock schema registry shared between SpecificAvroSerde instances
+		SchemaRegistryClient schemaRegistryClient = MockSchemaRegistry.getClientForScope(SCHEMA_REGISTRY_SCOPE);
 
 		// Dummy properties needed for test diver
 		Properties props = new Properties();
 		props.put(StreamsConfig.APPLICATION_ID_CONFIG, "test");
 		props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "dummy:1234");
 		props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, StringSerde.class);
-		props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, MockSchemaRegistryAvroSerde.class);
-		props.put(AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, "http://localhost:1234");
+		props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, SpecificAvroSerde.class);
+		props.put(AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, MOCK_SCHEMA_REGISTRY_URL);
 
 		// Create test driver
 		testDriver = new TopologyTestDriver(topology, props);
 
 		// Create Serdes used for test record keys and values
 		Serde<String> stringSerde = Serdes.String();
-		Serde<User> avroUserSerde = new MockSchemaRegistryAvroSerde<>();
-		Serde<Color> avroColorSerde = new MockSchemaRegistryAvroSerde<>();
+		Serde<User> avroUserSerde = new SpecificAvroSerde<>();
+		Serde<Color> avroColorSerde = new SpecificAvroSerde<>();
+
+		// Configure Serdes to use the same mock schema registry URL
+		Map<String, String> config = Map.of(
+				AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, MOCK_SCHEMA_REGISTRY_URL);
+		avroUserSerde.configure(config, false);
+		avroColorSerde.configure(config, false);
 
 		// Define input and output topics to use in tests
 		usersTopic = testDriver.createInputTopic(
@@ -64,6 +76,7 @@ class TopologyTestDriverAvroApplicationTests {
 	@AfterEach
 	void afterEach() {
 		testDriver.close();
+		MockSchemaRegistry.dropScope(SCHEMA_REGISTRY_SCOPE);
 	}
 
 	@Test
@@ -80,16 +93,4 @@ class TopologyTestDriverAvroApplicationTests {
 		assertTrue(colorsTopic.isEmpty());
 	}
 
-	/**
-	 * {@link SpecificAvroSerde} for {@link User}. Contains a {@link MockSchemaRegistryClient} shared between all
-	 * instances; needed since both our topology as well as our input and output topics need an instance.
-	 */
-	public static class MockSchemaRegistryAvroSerde<T extends SpecificRecord> extends SpecificAvroSerde<T> {
-		private static final MockSchemaRegistryClient schemaRegistry = new MockSchemaRegistryClient();
-
-		public MockSchemaRegistryAvroSerde() {
-			super(schemaRegistry);
-			configure(Map.of(AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, "http://localhost:1234"), false);
-		}
-	}
 }
