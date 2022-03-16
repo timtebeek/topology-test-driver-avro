326f34a0e6086667bdad80213ff535c3d7787ed2
Tim te Beek <tim.te.beek@jdriven.com>
2022-03-16 17:20:58 +0100

    Replace AbstractKafkaAvroSerDeConfig with AbstractKafkaSchemaSerDeConfig

--- src/test/java/com/github/timtebeek/TopologyTestDriverAvroApplicationTests.java
+++ src/test/java/com/github/timtebeek/TopologyTestDriverAvroApplicationTests.java
@@ -6,12 +6,17 @@ import java.util.Properties;
 import example.avro.Color;
 import example.avro.User;
 import io.confluent.kafka.schemaregistry.testutil.MockSchemaRegistry;
-import io.confluent.kafka.serializers.AbstractKafkaAvroSerDeConfig;
+import io.confluent.kafka.serializers.AbstractKafkaSchemaSerDeConfig;
 import io.confluent.kafka.streams.serdes.avro.SpecificAvroSerde;
 import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.common.serialization.Serdes.StringSerde;
-import org.apache.kafka.streams.*;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.TestInputTopic;
+import org.apache.kafka.streams.TestOutputTopic;
+import org.apache.kafka.streams.Topology;
+import org.apache.kafka.streams.TopologyTestDriver;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -30,6 +35,7 @@ class TopologyTestDriverAvroApplicationTests {
 	private TestOutputTopic<String, Color> colorsTopic;
 
 	@BeforeEach
+	@SuppressWarnings("resource")
 	void beforeEach() throws Exception {
 		// Create topology to handle stream of users
 		StreamsBuilder builder = new StreamsBuilder();
@@ -42,7 +48,7 @@ class TopologyTestDriverAvroApplicationTests {
 		props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "dummy:1234");
 		props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, StringSerde.class);
 		props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, SpecificAvroSerde.class);
-		props.put(AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, MOCK_SCHEMA_REGISTRY_URL);
+		props.put(AbstractKafkaSchemaSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, MOCK_SCHEMA_REGISTRY_URL);
 
 		// Create test driver
 		testDriver = new TopologyTestDriver(topology, props);
@@ -54,7 +60,7 @@ class TopologyTestDriverAvroApplicationTests {
 
 		// Configure Serdes to use the same mock schema registry URL
 		Map<String, String> config = Map.of(
-				AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, MOCK_SCHEMA_REGISTRY_URL);
+				AbstractKafkaSchemaSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, MOCK_SCHEMA_REGISTRY_URL);
 		avroUserSerde.configure(config, false);
 		avroColorSerde.configure(config, false);
 
